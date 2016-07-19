f49b75f4484489d549a4842e2c90c11cf6f78881
Matt Mitchell <mmitche@microsoft.com>
2016-07-19 12:52:49 -0700

    Add test summary steps

--- netci.groovy
+++ netci.groovy
@@ -1,6 +1,6 @@
 // Import the utility functionality.
 
-import jobs.generation.Utilities;
+import jobs.generation.*;
 
 def project = GithubProject
 def branch = GithubBranchName
@@ -9,9 +9,21 @@ def branch = GithubBranchName
     ['A', 'B', 'C', 'D', 'E', 'F'].each { letter ->
         def newJob = job(Utilities.getFullJobName(project, "innerloop_${letter}", isPR)) {
             steps {
-                batchFile("build.cmd")
+                batchFile("echo https://www.google.com > links1.txt")
+                batchFile("echo https://www.github.com >> links1.txt")
+                batchFile("echo https://www.microsoft.com >> links1.txt")
+                
+                batchFile("echo https://www.bing.com > links2.txt")
+                batchFile("echo https://www.reddit.com >> links2.txt")
+                batchFile("echo https://www.facebook.com >> links2.txt")
             }
         }
+        
+        // Emit summaries
+        SummaryBuilder summaries = new SummaryBuilder()
+        summaries.addLinksSummaryFromFile('Crash dumps from this run', 'links1.txt')
+        summaries.addLinksSummaryFromFile('Uncrash dumps from this run', 'links2.txt')
+        summaries.emit(newJob)
         
         Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
         Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
